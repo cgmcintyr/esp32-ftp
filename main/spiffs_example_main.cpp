218f991bcb7f0b2ecce528feadb0bfdebd90d7d0
Christopher McIntyre <me@cgmcintyre.com>
2017-12-16 19:39:58 +0000

    Use arduino-esp32 SPIFFS for setup

--- main/spiffs_example_main.cpp
+++ main/spiffs_example_main.cpp
@@ -13,42 +13,16 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_spiffs.h"
+#include "SPIFFS.h"
 
 static const char *TAG = "example";
 
 void setup()
 {
-    ESP_LOGI(TAG, "Initializing SPIFFS");
-
-    esp_vfs_spiffs_conf_t conf = {
-      .base_path = "/spiffs",
-      .partition_label = NULL,
-      .max_files = 5,
-      .format_if_mount_failed = true
-    };
-
-    // Use settings defined above to initialize and mount SPIFFS filesystem.
-    // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%d)", ret);
-        }
-        return;
-    }
-
-    size_t total = 0, used = 0;
-    ret = esp_spiffs_info(NULL, &total, &used);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get SPIFFS partition information");
-    } else {
-        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-    }
+  if(!SPIFFS.begin(true, "/spiffs", 5)){
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
 }
 
 extern "C" void app_main(void)
