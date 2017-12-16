7545bd1e6606ead7e43462a2ed010958c5d5233b
Christopher McIntyre <me@cgmcintyre.com>
2017-12-16 23:53:20 +0000

    Setup esp to act as station

--- main/spiffs_example_main.cpp
+++ main/spiffs_example_main.cpp
@@ -10,69 +10,43 @@
 #include <string.h>
 #include <sys/unistd.h>
 #include <sys/stat.h>
+#include <WiFi.h>
+#include <HardwareSerial.h>
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_spiffs.h"
 #include "SPIFFS.h"
+#include "nvs_flash.h"
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASS";
 
 static const char *TAG = "example";
 
 void setup()
 {
+  Serial.begin(115200);
+
   if(!SPIFFS.begin(true, "/spiffs", 5)){
     Serial.println("SPIFFS Mount Failed");
     return;
   }
+
+  nvs_flash_init();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 extern "C" void app_main(void)
 {
     setup();
-
-    // Use POSIX and C standard library functions to work with files.
-    // First create a file.
-    ESP_LOGI(TAG, "Opening file");
-    FILE* f = fopen("/spiffs/hello.txt", "w");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for writing");
-        return;
-    }
-    fprintf(f, "Hello World!\n");
-    fclose(f);
-    ESP_LOGI(TAG, "File written");
-
-    // Check if destination file exists before renaming
-    struct stat st;
-    if (stat("/spiffs/foo.txt", &st) == 0) {
-        // Delete it if it exists
-        unlink("/spiffs/foo.txt");
-    }
-
-    // Rename original file
-    ESP_LOGI(TAG, "Renaming file");
-    if (rename("/spiffs/hello.txt", "/spiffs/foo.txt") != 0) {
-        ESP_LOGE(TAG, "Rename failed");
-        return;
-    }
-
-    // Open renamed file for reading
-    ESP_LOGI(TAG, "Reading file");
-    f = fopen("/spiffs/foo.txt", "r");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for reading");
-        return;
-    }
-    char line[64];
-    fgets(line, sizeof(line), f);
-    fclose(f);
-    // strip newline
-    char* pos = strchr(line, '\n');
-    if (pos) {
-        *pos = '\0';
-    }
-    ESP_LOGI(TAG, "Read from file: '%s'", line);
-
-    // All done, unmount partition and disable SPIFFS
-    esp_vfs_spiffs_unregister(NULL);
-    ESP_LOGI(TAG, "SPIFFS unmounted");
 }
